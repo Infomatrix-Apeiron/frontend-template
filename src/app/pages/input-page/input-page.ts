cacc6914fdacacf1e9d3206efc489f93fd059a49
v.turenko <vitalii.turenko@dah-online.com>
2026-03-13 22:42:39 +0200

    input page: select file

--- src/app/pages/input-page/input-page.ts
+++ src/app/pages/input-page/input-page.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Button} from 'primeng/button';
+import {ApiService} from '../../_services/api.service';
+
+interface GenerateIdeasRequest {
+    prompt: string;
+    files: File[];
+}
 
 @Component({
-  selector: 'app-input-page',
-  imports: [],
-  templateUrl: './input-page.html',
-  styleUrl: './input-page.css',
+    selector: 'app-input-page',
+    imports: [
+        Button
+    ],
+    templateUrl: './input-page.html',
+    styleUrl: './input-page.css',
 })
-export class InputPage {
 
+export class InputPage {
+    selectedRequest?: GenerateIdeasRequest;
+
+    constructor(
+        private api: ApiService,
+    ) {
+    }
+
+    onFileSelected(event: Event) {
+
+        const input = event.target as HTMLInputElement;
+        const file = input.files?.[0];
+
+        if (!file) return;
+
+        console.log('file', file);
+
+        this.selectedRequest = {
+            prompt: '',
+            files: [file]
+        };
+    }
+
+    submit() {
+
+        if (!this.selectedRequest) return;
+
+        this.api.generateIdeas(
+            this.selectedRequest.prompt,
+            this.selectedRequest.files
+        ).subscribe();
+    }
 }
